e8b91daaffb502d0d91f0b6f5358bcc933280c9f
Sahid Kumar <119589370+Sahid18102@users.noreply.github.com>
2023-01-08 20:22:32 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,3 +1,16 @@
-# Demo
+package Array;
 
-Some description.
+public interface BigNumber
+{
+    public static void main(String[] args)
+    {
+        int ar[]={7,9,63,69,55,41};
+        int big=ar[0];
+        for(int i=0;i<ar.length;i++)
+        {
+            if(big<ar[i])
+            big=ar[i];
+        }
+        System.out.println("Big Number: "+big);
+    }
+}
